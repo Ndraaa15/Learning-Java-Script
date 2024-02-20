8399dd39ce7ded998d16f2c20f226864a2f1d684
Gede Indra Adi Brata <112854205+Ndraaa15@users.noreply.github.com>
2024-02-20 11:21:52 +0700

    Delete basic-java-script directory

--- basic-java-script/FetchRefac.js
+++
@@ -1,2 +0,0 @@
-// Diperjelas di callback2
-// Fetch refactoring sendiri berfungsi untuk membuat lebih rapi kode yang dibuat menjadi beberapa fungsi.
--- basic-java-script/errorHandling.js
+++
@@ -1,3 +0,0 @@
-// resolve ketika promisesnya berhasil
-// reject ketika promiesnya tidak berhasil
-// untuk error hadling pada async function menggunakan try and catch
